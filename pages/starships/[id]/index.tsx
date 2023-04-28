2b94193881b73b63d41b983428323c63f1fffe3a
Aygün Bayır <turkoaygun@gmail.com>
2023-04-29 01:02:33 +0300

    Fix header background

--- pages/starships/[id]/index.tsx
+++ pages/starships/[id]/index.tsx
@@ -1,7 +1,44 @@
-import React from "react";
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { fetchStarships } from '@/services/swapi'
+import { Starship } from '@/types/starship'
+import { Starships } from '@/data/Starships'
 
-function StarshipDetail() {
-  return <div>Starship Detail</div>;
+type StarshipDetailProps = {
+  data: Starship[]
 }
 
-export default StarshipDetail;
+function StarshipDetail({ data }: StarshipDetailProps) {
+  const router = useRouter()
+  const { id } = router.query
+
+  // @ts-ignore
+  const starship = data[parseInt(id)]
+
+  return (
+    <div>
+      <Link href="/">Back to Starships (sola yaslanacak)</Link>
+      Starship Detail
+      <h1>{starship.name}</h1>
+    </div>
+  )
+}
+
+export async function getStaticProps() {
+  return {
+    props: {
+      data: Starships,
+    },
+  }
+}
+
+export async function getStaticPaths() {
+  const paths = []
+  for (let i = 0; i < 36; i++) {
+    paths.push({ params: { id: i.toString() } })
+  }
+
+  return { paths, fallback: false }
+}
+
+export default StarshipDetail
