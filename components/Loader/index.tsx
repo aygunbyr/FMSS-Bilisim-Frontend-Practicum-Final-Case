a9e207b16f081f7a0297ffa20df6002c87e7b249
Aygün Bayır <turkoaygun@gmail.com>
2023-04-29 20:06:10 +0300

    Add loader

---
+++ components/Loader/index.tsx
@@ -0,0 +1,11 @@
+import styles from '@/styles/Loader.module.css'
+
+function Loader() {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.loader}></div>
+    </div>
+  )
+}
+
+export default Loader
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,33 @@
+import { useState, useEffect } from 'react'
+import Router from 'next/router'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
 import { StarshipsProvider } from '@/contexts/StarshipsContext'
+import Loader from '@/components/Loader'
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(() => {
+    Router.events.on('routeChangeStart', (url) => {
+      setIsLoading(true)
+    })
+
+    Router.events.on('routeChangeComplete', (url) => {
+      setIsLoading(false)
+    })
+
+    Router.events.on('routeChangeError', (url) => {
+      setIsLoading(false)
+    })
+  }, [Router])
+
   return (
     <>
       <StarshipsProvider>
         <Layout>
+          {isLoading && <Loader />}
           <Component {...pageProps} />
         </Layout>
       </StarshipsProvider>
