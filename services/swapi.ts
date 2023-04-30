9cf2ffbb9bc4c4d7ecda6c4f495f6715e1652fae
Aygün Bayır <turkoaygun@gmail.com>
2023-04-30 16:49:27 +0300

    Delete unused files

--- services/swapi.ts
+++ services/swapi.ts
@@ -1,10 +1,5 @@
 import axios from "axios";
 
-export const fetchPlanets = async (page: number) => {
-  const {data} = await axios.get(`https://swapi.dev/api/planets?page=${page}`)
-
-  return data;
-}
 
 export const fetchCharacters = async (page: number) => {
   const {data} = await axios.get(`https://swapi.dev/api/people?page=${page}`)
@@ -17,22 +12,3 @@ export const fetchStarships = async (page: number) => {
 
   return data;
 }
-
-export const fetchVehicles = async (page: number) => {
-  const { data } = await axios.get(`https://swapi.dev/api/vehicles?page=${page}`)
-
-  return data;
-}
-
-export const fetchSpecies = async (page: number) => {
-  const { data } = await axios.get(`https://swapi.dev/api/species?page=${page}`)
-
-  return data;
-}
-
-export const fetchFilms = async (page: number) => {
-  const { data } = await axios.get(`https://swapi.dev/api/films?page=${page}`)
-
-  return data;
-}
-
